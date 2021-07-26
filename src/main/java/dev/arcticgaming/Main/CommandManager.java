faea23d69ae5198e55256d0525059e26d37f6832
Arctic <43667891+ArcticRaven@users.noreply.github.com>
2021-07-25 20:01:51 -0400

    Add files via upload

--- src/main/java/dev/arcticgaming/Main/CommandManager.java
+++ src/main/java/dev/arcticgaming/Main/CommandManager.java
@@ -22,41 +22,86 @@ public class CommandManager implements CommandExecutor {
         if (cmd.getName().equalsIgnoreCase((cmd1))) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (args.length == 0) {
-                    player.sendMessage(ChatColor.RED + "Dice » Invalid command! " + ChatColor.WHITE + "Use /roll <number of dice> d<number of faces>");
-                } else{
-                    //establish unchecked vars
+
+                //calls external classes
+                ModifierCheck checkModifier = new ModifierCheck();
+                RollSum rollTotal = new RollSum();
+
+                //controls for roll modifiers
+                boolean modifierPresent = false;
+
+                //check if modifier is present
+                if (args.length < 3) {
+                    checkModifier.setModifier(0);
+                    modifierPresent = false;
+                    //debug message: player.sendMessage("modifier not found by command, setting to 0");
+
+                    //if modifier is NOT present, set modifier to 0
+                }
+                if (args.length == 3) {
+                    modifierPresent = true;
+                    boolean checkMod = new IntegerValidation().isInteger(args[2]);
+                    if (checkMod) {
+                        //if modifier is a valid integer, parse it as the roll modifier value
+                        checkModifier.setModifier(Integer.parseInt(args[2]));
+                        // debug message: player.sendMessage("modifier found! its " + checkModifier.getModifier());
+                    } else {
+                        // if modifier is not a valid integer, set it to 0
+                        player.sendMessage(ChatColor.GOLD + "Dice » Warning, Invalid modifier - setting to 0");
+                        checkModifier.setModifier(0);
+                    }
+                }
+                //check if arguments are missing, don't make modifier a requirement as its handled seperately
+                if (args.length < 2) {
+                    player.sendMessage(ChatColor.RED + "Dice » Error - Missing Arguments - /roll <# dice> d<# faces> <optional:modifier>");
+                } else {
+                    // if arguments are present, validate integers
                     boolean numDice = new IntegerValidation().isInteger(args[0]);
                     boolean numFace = new IntegerValidation().isInteger(args[1].substring(1));
-                    //if checked vars pass
+                    //if modifier was found, validate integer
                     if (numDice && numFace) {
+
+                        //parse integers
                         int intDice = Integer.parseInt(args[0]);
                         int intFace = Integer.parseInt(args[1].substring(1));
-                        RollSum rollTotal = new RollSum();
 
-                        //returns player inputs to verify
-                        //player.sendMessage(ChatColor.WHITE + "Values are: number of dice: " + numDice + " faces : " + numFace);
-
-                        //method to create rolls list?
+                        //start array to track individual dice rolls
                         ArrayList<Integer> rolls = new ArrayList<Integer>();
 
-                        //creates a random value for each roll
+                        //loop to get value for each dice roll
                         while (intDice > 0) {
+
+                            //creates random seed
                             Random generator = new Random();
+                            //random num generation uses doubles, faces set max range, +1 to cover 0 base
+                            //note: weird conversion with doubles and ints, acts funny in any other configuration
                             double valDice = generator.nextInt(intFace) + 1;
+                            //cast generated number as an integer
                             int intValDice = (int) valDice;
+                            //add random value to the array list
                             rolls.add(intValDice);
 
+                            //passes values to RollSum class to be calculated
                             rollTotal.setRollSum(rollTotal.getRollSum(), intValDice);
+
+                            //subtracts one from amount of dice to be calculated
                             intDice = intDice - 1;
-                            /*returns values after EACH roll - debugging messages
-                             * player.sendMessage(ChatColor.YELLOW + "Dice Rolls: " + rolls);
-                             * player.sendMessage(ChatColor.GOLD + "Dice Sum: " + rollTotal.getRollSum()); */
+
+                                /*Debug messages to check values for each roll
+                                player.sendMessage(ChatColor.YELLOW + "Dice Rolls: " + rolls);
+                                player.sendMessage(ChatColor.GOLD + "Dice Sum: " + rollTotal.getRollSum());*/
                         }
-                        player.sendMessage(ChatColor.YELLOW + "Dice Rolls: " + rolls);
-                        player.sendMessage(ChatColor.GOLD + "Dice Sum: " + rollTotal.getRollSum());
+                        //returns the list of individual rolls to the player
+                        player.sendMessage(ChatColor.AQUA + "Dice » Values: " + ChatColor.WHITE + rolls);
+                        //returns the sum of dice WITHOUT modifiers
+                        if (args.length == 3) {
+                            player.sendMessage(ChatColor.AQUA + "Dice » Modifier: " + ChatColor.WHITE + checkModifier.getModifier() +ChatColor.AQUA + "\nDice » Modified Sum: " + ChatColor.WHITE + (rollTotal.getRollSum() + checkModifier.getModifier()));
+                        } else {
+                            //returns the sum of dice WITHOUT modifiers
+                            player.sendMessage(ChatColor.AQUA + "Dice » Sum: " + ChatColor.WHITE + rollTotal.getRollSum());
+                        }
                     } else {
-                        player.sendMessage(ChatColor.RED + "Dice » Invalid command! " + ChatColor.WHITE + "Use /roll <number of dice> d<number of faces>");
+                        player.sendMessage(ChatColor.RED + "Dice » Error! Invalid format, use /roll <# dice> d<# faces> <optional:modifier>");
                     }
                 }
             }
@@ -64,5 +109,3 @@ public class CommandManager implements CommandExecutor {
         return true;
     }
 }
-
-
---
+++ src/main/java/dev/arcticgaming/Main/ModifierCheck.java
@@ -0,0 +1,14 @@
+package dev.arcticgaming.Main;
+
+public class ModifierCheck {
+    private int modifier = 0; //established private var rollSum
+
+    public int getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(int newModifier) {
+        this.modifier = newModifier;
+    }
+
+}
